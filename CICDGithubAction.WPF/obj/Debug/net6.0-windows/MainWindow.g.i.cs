b8d58b930b52f5fd33908dcfd3a57db967534cf3
Kelly E. Osoba <sirkellyc@gmaail.com>
2023-03-08 09:26:34 +0100

    fixing publishing fail

--- CICDGithubAction.WPF/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ CICDGithubAction.WPF/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace CICDGithubAction.WPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CICDGithubAction.WPF;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CICDGithubAction.WPF;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
